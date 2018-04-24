013a316c36b02b075620e0f362ba5dcabb973be1
Michael Pfister <pfista@gmail.com>
2018-04-24 09:35:39 -0700

    update comments

--- challenge.py
+++ challenge.py
@@ -7,14 +7,15 @@ config = json.load(open('config.json'))
 def main():
     client = APIClient(config['NYLAS_APP_ID'], config['NYLAS_APP_SECRET'], config['NYLAS_ACCESS_TOKEN'])
 
-    # Get 10 threads from lars@metafoo.de
+    # Use `client` to get 10 threads "from" lars@metafoo.de and store them in a
+    # variable
 
     # Print out the subject of each of those threads
 
-    # Send a message to mike@nylas.com. The body should include all message_ids of
-    # the threads from above. 
+    # Send a message to inboxapptest415@gmail.com. The body should include all
+    # message_ids of the 10 threads from above, each separated by a ",".
 
-    # Print the Message-ID Header in the message you just sent
+    # Print the raw RFC-2822 Message-ID Header in the message you just sent
 
 if __name__ == '__main__':
     main()
--- readme.md
+++ readme.md
@@ -4,11 +4,13 @@
 
 To get started activate a virtualenv and then install dependencies with
 
-```
+```bash
+virtualenv .venv
+. .venv/bin/activate
 pip install -r requirements.txt
 ```
 
-Next, you'll need to update config.json with your specific credentials.
+Next, you'll need to update `config.json` with your specific credentials.
 
 ## Challenge
 
@@ -16,7 +18,7 @@ You'll be using the Nylas Python SDK to perform a few tasks.  You might want to
 reference our [Python SDK](https://github.com/nylas/nylas-python/) and our [API
 documentation](https://docs.nylas.com) when working on the challenge.
 
-To get started, open challenge.py in your preferred editor and follow the
+To get started, open `challenge.py` in your preferred editor and follow the
 instructions within the file.
 
 Good luck!
