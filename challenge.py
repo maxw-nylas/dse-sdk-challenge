d6eb08bf2f0603e79ac79f9ab8d3d9ff8aab64cd
maxw-nylas <max.w@nylas.com>
2022-05-04 10:24:03 -0600

    initial commit

--- challenge.py
+++ challenge.py
@@ -7,11 +7,11 @@ config = json.load(open('config.json'))
 def main():
     client = APIClient(config['NYLAS_CLIENT_ID'], config['NYLAS_CLIENT_SECRET'], config['NYLAS_ACCESS_TOKEN'])
 
-    # Use `client` to get 10 threads "from" nylastest1@nylatest.onmicrosoft.com and store them in a
+    # Use `client` to get 10 threads "from"max.w@nylas.com and store them in a
     # variable
 
     # Print out the subject of each of those threads
-
+    
     # Send a message to nylas.dse.challenge@gmail.com. The body should include all
     # message_ids of the 10 threads from above, each separated by a ",".
 
